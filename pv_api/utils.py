a13db534f1bd3d83b8d685c981fe8225f6c053f3
Georgi <georgi.manev@entra.energy>
2025-01-08 16:47:27 +0200

    first commit

--- pv_api/utils.py
+++ pv_api/utils.py
@@ -1,20 +1,31 @@
 import requests
 from .models import PvData
+from datetime import timedelta
+from datetime import datetime
+
 
 def fetch_and_store_pv_data():
     url = "https://geos-api-app.azurewebsites.net/Api?ApiLogin=PV_API&ApiKey=19F1A3B8-27B9-4147-ACB1-5BC786E419D4"
     response = requests.get(url)
     data = response.json()    
+    
     for item in data:
-        if not PvData.objects.filter(signal_time=item['signal_time'], signal_uid=item['signal_uid']).exists():
-            PvData.objects.create(
-                parameter_id=item['parameter_id'],
-                installation_name=item['installation_name'],
-                signal_uid=item['signal_uid'],
-                signal_time=item['signal_time'],
-                signal_value=item['signal_value'],
-                unit=item['unit'],
-            )
-        else:
-            # Handle the case where the record already exists
-            print(f"Record with signal_time {item['signal_time']} and signal_uid {item['signal_uid']} already exists.")
+        signal_time = datetime.strptime(item['signal_time'], "%Y-%m-%d %H:%M:%S")
+        for i in range(5):
+            minute_signal_time = signal_time - timedelta(minutes=i)
+            
+            signal_value = float(item['signal_value']) / 5 if item['parameter_id'] == 720 else item['signal_value']
+            
+            # Check for uniqueness
+            if not PvData.objects.filter(signal_time=minute_signal_time, signal_uid=item['signal_uid']).exists():
+                PvData.objects.create(
+                    parameter_id=item['parameter_id'],
+                    installation_name=item['installation_name'],
+                    signal_uid=item['signal_uid'],
+                    signal_time=minute_signal_time,
+                    signal_value=signal_value,
+                    unit=item['unit'],
+                )
+            else:
+                # Handle the case where the record already exists
+                print(f"Record with signal_time {minute_signal_time} and signal_uid {item['signal_uid']} already exists.")
