db70589ed381a22973f780bcdb05208603401954
Georgi <georgi.manev@entra.energy>
2025-01-13 17:30:41 +0200

    first commit

--- pv_api/views.py
+++ pv_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .models import PvTechnicalData, PvMeasurementData
 from django_filters.rest_framework import DjangoFilterBackend
+from datetime import timedelta
 from django.db.models import Max
 
 from .serializers import PvDataSerializer, PvMeasurementDataSerializer
@@ -22,17 +23,18 @@ class PvMeasurementDataViewSet(viewsets.ReadOnlyModelViewSet):
         Override get_queryset to dynamically filter by the month of the most recent data point.
         """
         queryset = super().get_queryset()
-        month = self.request.query_params.get('month')
-        if month:
+        week = self.request.query_params.get('week')
+        if week:
             # Get the most recent data point
             last_data_point = queryset.aggregate(Max('timestamp'))['timestamp__max']
             
             if last_data_point:
                 # Get the first day of the month for the last data point
                 last_data_point = last_data_point.replace(hour=0, minute=0, second=0, microsecond=0)
-                start_of_month = last_data_point.replace(day=1)  # First day of the last data point's month
+                # start_of_month = last_data_point.replace(day=1)  # First day of the last data point's month
+                one_wee_ago = last_data_point - timedelta(days=7)
                 
                 # Filter the data to return only records from the current month
-                queryset = queryset.filter(timestamp__gte=start_of_month)
+                queryset = queryset.filter(timestamp__gte=one_wee_ago)
 
         return queryset
