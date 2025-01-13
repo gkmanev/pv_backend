86917304819a6f317d104d4921b14d6a4b3dd69d
Georgi <georgi.manev@entra.energy>
2025-01-13 18:50:31 +0200

    first commit

--- pv_api/serializers.py
+++ pv_api/serializers.py
@@ -10,3 +10,10 @@ class PvMeasurementDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = PvMeasurementData
         fields = '__all__'
+
+class AggregatedPvMeasurementDataSerializer(serializers.Serializer):
+    day = serializers.DateField()
+    total_production = serializers.DecimalField(max_digits=10, decimal_places=3)
+    avg_temperature = serializers.DecimalField(max_digits=10, decimal_places=2)
+    total_uv_index = serializers.DecimalField(max_digits=10, decimal_places=2)
+    total_radiation = serializers.DecimalField(max_digits=10, decimal_places=2)
--- pv_api/views.py
+++ pv_api/views.py
@@ -5,9 +5,10 @@ from django.db.models import Max
 from django.db.models.functions import TruncDay
 from django.db.models import Sum
 from datetime import timedelta
-
-from .serializers import PvDataSerializer, PvMeasurementDataSerializer
+from .serializers import PvDataSerializer, PvMeasurementDataSerializer, AggregatedPvMeasurementDataSerializer
 from pv_api.filters import PvMeasurementDataFilter  # <-- Import the filter
+from rest_framework.response import Response
+
 
 class PvDataViewSet(viewsets.ModelViewSet):
     queryset = PvTechnicalData.objects.all().order_by('signal_time')
@@ -39,3 +40,11 @@ class PvMeasurementDataViewSet(viewsets.ReadOnlyModelViewSet):
             )
 
         return queryset
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        if 'all' in request.query_params:
+            serializer = AggregatedPvMeasurementDataSerializer(queryset, many=True)
+        else:
+            serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
