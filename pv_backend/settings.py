4266d2f2b62ecc7992dc5994cc06ad73f897c933
Georgi <georgi.manev@entra.energy>
2025-01-13 15:46:24 +0200

    first commit

--- pv_backend/settings.py
+++ pv_backend/settings.py
@@ -77,9 +77,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'pv_backend.urls'
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,  # Customize this based on your needs
 }
 
 TEMPLATES = [
