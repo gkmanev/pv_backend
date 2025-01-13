8bd9032f21f870f2df20477cceee51141f58e21f
Georgi <georgi.manev@entra.energy>
2025-01-13 17:40:30 +0200

    first commit

--- pv_api/views.py
+++ pv_api/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from .models import PvTechnicalData, PvMeasurementData
 from django_filters.rest_framework import DjangoFilterBackend
-from datetime import timedelta
 from django.db.models import Max
 
 from .serializers import PvDataSerializer, PvMeasurementDataSerializer
@@ -16,25 +15,23 @@ class PvMeasurementDataViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = PvMeasurementDataSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = PvMeasurementDataFilter 
-
     
     def get_queryset(self):
         """
         Override get_queryset to dynamically filter by the month of the most recent data point.
         """
         queryset = super().get_queryset()
-        week = self.request.query_params.get('week')
-        if week:
+        month = self.request.query_params.get('month')
+        if month:
             # Get the most recent data point
             last_data_point = queryset.aggregate(Max('timestamp'))['timestamp__max']
             
             if last_data_point:
                 # Get the first day of the month for the last data point
                 last_data_point = last_data_point.replace(hour=0, minute=0, second=0, microsecond=0)
-                # start_of_month = last_data_point.replace(day=1)  # First day of the last data point's month
-                one_wee_ago = last_data_point - timedelta(days=7)
+                start_of_month = last_data_point.replace(day=1)  # First day of the last data point's month
                 
                 # Filter the data to return only records from the current month
-                queryset = queryset.filter(timestamp__gte=one_wee_ago)
+                queryset = queryset.filter(timestamp__gte=start_of_month)
 
         return queryset
--- pv_backend/settings.py
+++ pv_backend/settings.py
@@ -79,8 +79,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'pv_backend.urls'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,  # Customize this based on your needs
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 100,  # Customize this based on your needs
 }
 
 TEMPLATES = [
