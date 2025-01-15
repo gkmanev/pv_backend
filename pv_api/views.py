003a47cf756857247236bb11138300e26827569e
Georgi <georgi.manev@entra.energy>
2025-01-15 15:16:17 +0200

    up

--- pv_api/views.py
+++ pv_api/views.py
@@ -61,6 +61,14 @@ class PvMeasurementDataViewSet(viewsets.ReadOnlyModelViewSet):
         return queryset
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            if 'all' in request.query_params:
+                serializer = AggregatedPvMeasurementDataSerializer(queryset, many=True)
+            else:
+                serializer = self.get_serializer(queryset, many=True)
+            return self.get_paginated_response(serializer.data)    
+            
         if 'all' in request.query_params:
             serializer = AggregatedPvMeasurementDataSerializer(queryset, many=True)
         else:
