7c71e68b58934b0d4e49d75f44e24dd4b7fccadf
Georgi <georgi.manev@entra.energy>
2025-01-16 11:08:42 +0200

    up

--- pv_api/views.py
+++ pv_api/views.py
@@ -19,20 +19,18 @@ class PvMeasurementDataViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = PvMeasurementData.objects.all().order_by('timestamp')
     serializer_class = PvMeasurementDataSerializer
     pagination_class = PageNumberPagination
-    
+
     def get_queryset(self):
-        """
-        Override get_queryset to dynamically filter by the month of the most recent data point.
-        """
         queryset = super().get_queryset()
         aggregate_all = self.request.query_params.get('all')
         day_ahead = self.request.query_params.get('day_ahead')
         farm = self.request.query_params.get('farm')
         start_date = self.request.query_params.get('start_date')
         end_date = self.request.query_params.get('end_date')
-        if aggregate_all:  # If 'all' query param is received            
+
+        if aggregate_all:
             queryset = (
-                queryset.annotate(day=TruncDate('timestamp'))  # Truncate to date instead of datetime
+                queryset.annotate(day=TruncDate('timestamp'))
                 .values('day', 'farm')
                 .annotate(
                     total_production=Sum('production'),
@@ -41,39 +39,21 @@ class PvMeasurementDataViewSet(viewsets.ReadOnlyModelViewSet):
                     avg_direct_radiation=Avg('direct_radiation', filter=~Q(direct_radiation=0)),
                     latitude=F('latitude'),
                     longitude=F('longitude')
-            )
+                )
                 .order_by('day', 'farm')
             )
-            if farm:
-                queryset = queryset.filter(farm=farm)
         elif day_ahead:
-            # Filter from today to the next day + 1 without aggregation
-            # get today's date
-            today = datetime.now().date()            
+            today = datetime.now().date()
             queryset = queryset.filter(timestamp__gte=today)
-        
-        elif start_date and end_date and farm:
-            # Filter by start_date, end_date and farm
-            queryset = queryset.filter(timestamp__range=[start_date, end_date], farm=farm)
-            
+        elif start_date and end_date:
+            queryset = queryset.filter(timestamp__range=[start_date, end_date])
 
-        else:
-            queryset = queryset
+        if farm:
+            queryset = queryset.filter(farm=farm)
 
         return queryset
-    
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            if 'all' in request.query_params:
-                serializer = AggregatedPvMeasurementDataSerializer(queryset, many=True)
-            else:
-                serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)    
-            
-        if 'all' in request.query_params:
-            serializer = AggregatedPvMeasurementDataSerializer(queryset, many=True)
-        else:
-            serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.request.query_params.get('all'):
+            return AggregatedPvMeasurementDataSerializer
+        return self.serializer_class
