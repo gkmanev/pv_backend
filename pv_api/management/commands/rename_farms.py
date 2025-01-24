05de4ea7686ddd11f2660d71bfadb94660c77665
Georgi <georgi.manev@entra.energy>
2025-01-24 15:38:43 +0200

    up

--- pv_api/management/commands/rename_farms.py
+++ pv_api/management/commands/rename_farms.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     help = 'Import data from combined_weather_and_df_dam.csv into PvMeasurementData'
 
     def handle(self, *args, **kwargs):
-        queryset = PvMeasurementData.objects.filter(timestamp__gte='2023-09-30', timestamp__lte='2023-12-31')
+        queryset = PvMeasurementData.objects.filter(timestamp__gte='2024-01-01', timestamp__lte='2024-03-31')
         project_mapping_path = os.path.join(settings.BASE_DIR, 'projects_mapping.json')
         project_mapping = []
         try:
